ddbcc7085b0d61987937892cb61f9484d45188f9
Yamato Kataoka <yk_time@icloud.com>
2018-08-25 09:29:06 +0000

    Added users index view and action details

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, only:[:new, edit]
+  before_action :authenticate_user!, only:[:new, :edit]
   before_action :only_current_user
   
   # GET to /users/:user_id/profile/new
